5b5ba553400c8e76915b00f30f65dd935b389e4c
Roger D. Peng <rdpeng@gmail.com>
2024-06-27 11:14:59 -0500

    Start using ROxygen

--- R/mvtsplot.R
+++ R/mvtsplot.R
@@ -1,16 +1,16 @@
 ################################################################################
 ## Copyright (C) 2008 Roger D. Peng <rpeng@jhsph.edu>
-## 
+##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
 ## the Free Software Foundation; either version 2 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -27,7 +27,7 @@ drawImage <- function(cx, pal, nlevels, xlim, xtime, group, gcol) {
         side2 <- 0.2
 
         ## Setup image plot
-        if(!is.null(cn)) 
+        if(!is.null(cn))
                 side2 <- max(side2, max(strwidth(cn, "inches")) + 0.1)
         else
                 cn <- as.character(1, nc)
@@ -241,7 +241,9 @@ nalines <- function(x, y, NAcol = gray(0.6), ...) {
         invisible()
 }
 
-
+#' mvtsplot
+#'
+#'
 mvtsplot <- function(x, group = NULL, xtime = NULL,
                      norm = c("internal", "global"), levels = 3,
                      smooth.df = NULL, margin = TRUE,
